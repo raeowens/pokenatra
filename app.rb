c028515766b804255045e88a9c4024fc123663ed
Doray Gisele Owens <doray.owens@gmail.com>
2016-10-05 16:31:04 -0400

    final changes

--- app.rb
+++ app.rb
@@ -13,3 +13,24 @@ end
 get "/pokemons/new" do
   erb :"pokemons/new"
 end
+
+get "/pokemons/:id/edit" do
+  @pokemons = Pokemon.find(params[:id])
+  erb :"pokemons/edit"
+end
+
+put "/pokemons/:id" do
+  @pokemons = Pokemon.find(params[:id])
+  @pokemons.update(params[:pokemons])
+  redirect "/pokemons/#{@pokemon.id}"
+end
+
+post "/pokemons" do
+  @pokemons = Pokemon.create(params[:pokemon])
+  redirect "/pokemons/#{@pokemon.id}"
+end
+
+get "/pokemons/:id" do
+  @pokemons = Pokemon.find(params[:id])
+  erb :"pokemons/show"
+end
