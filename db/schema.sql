fc0eeb58a7a8c3fcff4ffeb77e9a09c1272664af
Doray Gisele Owens <doray.owens@gmail.com>
2016-10-05 13:47:07 -0400

    Added gem and models for pokemon and trainer

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,17 @@
 DROP TABLE IF EXISTS pokemons;
+DROP TABLE IF EXISTS trainers;
 
 CREATE TABLE pokemons (
   id SERIAL PRIMARY KEY,
   name TEXT,
   poke_type TEXT,
   cp TEXT,
+  img_url TEXT
+);
+
+CREATE TABLE trainers (
+  id SERIAL PRIMARY KEY,
+  name TEXT,
+  level TEXT,
   img_url TEXT
 );
